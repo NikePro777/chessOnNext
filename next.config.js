25abfd0fc436d228b07218b30a28594cbf2a372c
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-03-22 12:05:52 +0500

    file settings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+	poweredByHeader: false,
 	env: {
-		SERVER_URL: process.env.SERVER_URL,
-		APP_URL: process.env.APP_URL
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
 	},
-	images: {
-		domains: ['loremflickr.com', 'www.aptronixindia.com', 'cloudflare-ipfs.com']
-	}
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:4200/uploads/:path*`,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
