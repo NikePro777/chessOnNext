dd808ec18684bab0dac770c3f5ed1153f5d7a333
evgeniy.datskiy <evgenij.datskij@mail.ru>
2023-03-25 17:49:33 +0500

    start menu

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,9 +6,12 @@ import Sidebar from './Sidebar/Sidebar'
 
 const Layout: React.FC = ({ children }) => {
 	return (
-		<div className={styles.layout}>
-			<h1>Layout</h1>
-			<Navigation />
+		<div className="wrapper">
+			<header>
+				<div className="header">
+					<Navigation />
+				</div>
+			</header>
 			<div className={styles.center}>child {children} child2</div>
 			<main>{children}</main>
 			<Sidebar />
---
+++ src/components/Layout/Navigation/MenuContainer/MenuContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './MenuContainer.module.scss'
+
+const MenuContainer: React.FC = () => {
+	return (
+		<nav>
+			<ul className="menu"></ul>
+		</nav>
+	)
+}
+
+export default MenuContainer
