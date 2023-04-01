f8913d8e8ee6f1883f0092924f7d68b216982e46
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-04-01 17:25:36 +0500

    add header-menu

--- src/components/screens/home/banner/banner.tsx
+++ src/components/screens/home/banner/banner.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { FC } from 'react'
 
 import styles from './header_banner.module.scss'
@@ -9,22 +10,22 @@ const Banner: FC = () => {
 				{/* <div className="header-banner"> */}
 				<div className="container">
 					<div className={styles.header_banner__items}>
-						<div className="header-banner__item">
-							<h1 className="header-banner__title">ШахМатOff</h1>
-							<p className="header-banner__subtitle">
+						<div className={styles.header_banner__item}>
+							<h1 className={styles.header_banner__title}>ШахМатOff</h1>
+							<p className={styles.header_banner__subtitle}>
 								БОЛЬШЕ, ЧЕМ ПРОСТО ШАХМАТНАЯ ШКОЛА
 							</p>
 						</div>
-						<div className="header-banner__item">
+						<div className={styles.header_banner__item}>
 							<a
 								href="tel:+73432712011"
-								className="header-banner__content_tell"
+								className={styles.header_banner__content_tell}
 							>
 								+7 (343) 271-20-11
 							</a>
-							<a href="./zakazat-zvonok.html" className="btn">
+							<Link href="./" className="btn">
 								Оформить заявку сейчас
-							</a>
+							</Link>
 						</div>
 					</div>
 				</div>
